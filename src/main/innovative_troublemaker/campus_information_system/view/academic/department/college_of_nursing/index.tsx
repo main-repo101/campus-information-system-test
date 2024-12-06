ee028d6f57659c4c5d37b75c8ff5424a8d134414
jayoadonis <jayo.adonisraphael@gmail.com>
2024-12-06 09:53:47 +0800

    [chore: 0.0.0] .|. Update College of Nursing

--- src/main/innovative_troublemaker/campus_information_system/view/academic/department/college_of_nursing/index.tsx
+++ src/main/innovative_troublemaker/campus_information_system/view/academic/department/college_of_nursing/index.tsx
@@ -2,20 +2,21 @@
 import * as React from "react";
 import * as MUI from "@mui/material";
 
-import BG_HERO_001
-    from "@innovative_troublemaker/campus_information_system/resource/img/department/college_of_engineering/bg-hero-001.jpg";
-import CONTEXT_0x0003, { IStateContextType } from "@innovative_troublemaker/campus_information_system/component/layout/context/Context0x0003.tsx";
 
-
-export default function CollegeOfNursing(): React.ReactElement {
-
-    const CONTEXT: IStateContextType | undefined
-        = React.useContext(CONTEXT_0x0003);
-
-    return (<>
-        <MUI.Typography variant="innovativeTroublemakerH2"
-            className={`text-lime-500`}>
-            College of Nursing
-        </MUI.Typography>
+const ViewOSA: React.FC = () => {
+    return(<>
+        <div style={{ width: '100%', height: '100vh' }}>
+            <iframe
+                src="/resources/academic/department/college_of_nursing/index.html"
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    border: 'none',
+                }}
+                title="Embedded HTML"
+            ></iframe>
+        </div>
     </>);
 }
+
+export default ViewOSA;
